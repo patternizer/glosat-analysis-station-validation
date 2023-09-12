eb61cb29bb1353a215f4313c61d5b82174eedb87
patternizer <michael.a.taylor@uea.ac.uk>
2023-09-12 19:01:10 +0100

    Code tidy up. Animated gif of monthly maps code.

--- make_gif.py
+++ make_gif.py
@@ -26,6 +26,7 @@ import imageio.v2 as imageio
 use_reverse_order = False
 png_dir = 'tas_median_20*.png'
 gif_str = 'tas_median_20xx.gif'
+mp4_str = 'tas_median_20xx.mp4'
 
 if use_reverse_order == True:
     a = glob.glob(png_dir)
@@ -46,8 +47,10 @@ imageio.mimsave(gif_str, var, fps = 10)
 # GIF --> MP4
 # ffmpeg -i gif_str -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" mp4_str
 
-#sudo apt install gifsicle
-#gifsicle  g1.gif  g2.gif  g3.gif > combo.gif 
+# MERGE: century level GIFs (if too large for convert)
+
+# sudo apt install gifsicle
+# gifsicle  g1.gif  g2.gif  g3.gif > combo.gif 
 
 # -----------------------------------------------------------------------------
 print('** END')
